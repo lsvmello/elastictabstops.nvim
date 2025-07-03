e5ae8bb994484f4056c5a672fa6de9e73f344d2f
Leonardo Mello <lsvmello@gmail.com>
2025-07-03 10:16:19 -0300

    fix: logic when there are over 3 tabs
    fixes #1

--- lua/elastictabstops/init.lua
+++ lua/elastictabstops/init.lua
@@ -9,17 +9,29 @@ local function create_extmark(bufnr, line, column, virt_text, virt_text_pos)
   })
 end
 
+local function get_virt_col_sum(tabs, line_idx, tab_idx)
+  if tabs[line_idx] == nil or tabs[line_idx][tab_idx] == nil then
+    return 0
+  end
+
+  return tabs[line_idx][tab_idx].virt_col + get_virt_col_sum(tabs, line_idx, tab_idx - 1)
+end
+
+
 local function get_max_tab_column_index(tabs, line_idx, tab_idx, step)
   if tabs[line_idx] == nil or tabs[line_idx][tab_idx] == nil then
     return -1
   end
 
   local tab_pos = tabs[line_idx][tab_idx]
-  return math.max(tab_pos.col + tab_pos.virt_col, get_max_tab_column_index(tabs, line_idx + step, tab_idx, step))
+  return math.max(
+    tab_pos.col + tab_pos.virt_col + get_virt_col_sum(tabs, line_idx, tab_idx - 1),
+    get_max_tab_column_index(tabs, line_idx + step, tab_idx, step))
 end
 
-local function get_higher_adjacent_tab_column(tabs, line_idx, tab_idx)
-  return math.max(get_max_tab_column_index(tabs, line_idx, tab_idx, 1),
+local function get_farthest_adjacent_tab_column(tabs, line_idx, tab_idx)
+  return math.max(
+    get_max_tab_column_index(tabs, line_idx, tab_idx, 1),
     get_max_tab_column_index(tabs, line_idx, tab_idx, -1))
 end
 
@@ -28,26 +40,44 @@ local function align_tab_index(bufnr, tabs, line_idx, tab_idx)
     return false
   end
 
-  local longestAdjacentColumn = get_higher_adjacent_tab_column(tabs, line_idx, tab_idx)
+  local farthest_adjacent_column = get_farthest_adjacent_tab_column(tabs, line_idx, tab_idx)
   local tab_pos = tabs[line_idx][tab_idx]
-  if longestAdjacentColumn > tab_pos.col + tab_pos.virt_col then
-    local distance = longestAdjacentColumn - tab_pos.col - tab_pos.virt_col
-    create_extmark(bufnr, line_idx, tab_pos.col + 1, string.rep(' ', distance), 'inline')
-    tab_pos.virt_col = tab_pos.virt_col + distance
-    -- update next tabs virt_col if any
-    if tabs[line_idx][tab_idx + 1] ~= nil then
-      for i = tab_idx + 1, #tabs[line_idx] do
-        tabs[line_idx][i].virt_col = tabs[line_idx][i].virt_col + tab_pos.virt_col
-      end
-    end
+  local virt_col_sum = get_virt_col_sum(tabs, line_idx, tab_idx - 1)
+  if farthest_adjacent_column > tab_pos.col + virt_col_sum then
+    tab_pos.virt_col = farthest_adjacent_column - tab_pos.col - virt_col_sum
+    create_extmark(bufnr, line_idx - 1, tab_pos.col, string.rep(' ', tab_pos.virt_col), 'inline')
   end
   return true
 end
 
-local function add_elastic_tab_stop(bufnr, tabs)
+local function clear_elastic_tabstops(bufnr)
+  vim.api.nvim_buf_clear_namespace(bufnr, ets_ns, 0, -1)
+end
+
+local function render_elastic_tabstops(bufnr)
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+
+  -- replace tab nil character
+  local tabs = {}
+  for line_idx, line in ipairs(lines) do
+    local tab_idx = 1
+    local tab_list = {}
+    local line_bytes = { line:byte(1, #line) }
+    for column = 1, #line_bytes do
+      if string.char(line_bytes[column]) == '\t' then
+        create_extmark(bufnr, line_idx - 1, column - 1, '  ', 'overlay')
+        tab_list[tab_idx] = { col = column, virt_col = 0 }
+        tab_idx = tab_idx + 1
+      end
+    end
+    if tab_idx > 1 then
+      tabs[line_idx] = tab_list
+    end
+  end
+
+  -- add elastic tabstops
   local tab_idx = 0
   local try_next = true
-
   while try_next do
     tab_idx = tab_idx + 1
     try_next = false
@@ -57,28 +87,7 @@ local function add_elastic_tab_stop(bufnr, tabs)
       try_next = try_next or found
     end
   end
-end
 
-local function replace_tabs(bufnr)
-  local tabs = {}
-  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  for line_idx, line in ipairs(lines) do
-    local tab_idx = 1
-    local tab_list = {}
-    local line_bytes = { line:byte(1, #line) }
-    for column = 1, #line_bytes do
-      if string.char(line_bytes[column]) == '\t' then
-        create_extmark(bufnr, line_idx - 1, column - 1, '  ', 'overlay')
-        tab_list[tab_idx] = { col = column - 1, virt_col = 0 }
-        tab_idx = tab_idx + 1
-      end
-    end
-    if tab_idx > 1 then
-      tabs[line_idx - 1] = tab_list
-    end
-  end
-
-  add_elastic_tab_stop(bufnr, tabs)
 end
 
 local M = {}
@@ -95,15 +104,15 @@ M.setup = function()
 
     local bufnr = vim.api.nvim_get_current_buf()
     -- create extmarks
-    replace_tabs(bufnr)
+    render_elastic_tabstops(bufnr)
 
     -- set autocommands
     vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
       group = ets_group,
       buffer = bufnr,
       callback = function(ev)
-        vim.api.nvim_buf_clear_namespace(ev.buf, ets_ns, 0, -1)
-        replace_tabs(ev.buf)
+        clear_elastic_tabstops(ev.buf)
+        render_elastic_tabstops(ev.buf)
       end
     })
   end, {})
@@ -116,7 +125,7 @@ M.setup = function()
 
     local bufnr = vim.api.nvim_get_current_buf()
     -- clear extmarks
-    vim.api.nvim_buf_clear_namespace(bufnr, ets_ns, 0, -1)
+    clear_elastic_tabstops(bufnr)
 
     -- remove autocommands
     local autocommands = vim.api.nvim_get_autocmds({
